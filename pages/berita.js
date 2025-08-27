0924a6a0288630d72de424e450e503435cc1deab
Yugaaa-Jago <fakihartayuga899@gmail.com>
2025-08-27 19:34:51 +0700

    tambah halaman berita & home

---
+++ pages/berita.js
@@ -0,0 +1,7 @@
+export default function Berita() {
+    return (
+        <main className="flex justify-center items-center min-h-screen bg-slate-100">
+            <h1 className="text-3xl font-bold text-sky-700">Halaman Berita TKJ</h1>
+        </main>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main className="flex justify-center items-center min-h-screen bg-slate-100">
+            <h1 className="text-3xl font-bold text-sky-700">
+                Selamat Datang di TKJ Web SMKN 1 BLADO
+            </h1>
+        </main>
+    );
+}
